80c205af175408af1de9134f01ed353985c6836e
Walid Bouladam <bouladam.walid86@gmail.com>
2022-01-29 22:01:07 +1300

    Able to accept second num input

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Enter a number: ");
 int num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter another number: ");
+int num2 = int.Parse(Console.ReadLine());
+
 
 Console.ReadLine();
