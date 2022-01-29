ff01bc7ee06ba1efd02dcc6bac28a3df6163bfae
Walid Bouladam <bouladam.walid86@gmail.com>
2022-01-29 22:06:49 +1300

    Addeed double vars in ordder for user to be able to add values that possess decimals

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Add integers (whole numbers) only
 Console.Write("Enter a number: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Enter another number: ");
 int num2 = int.Parse(Console.ReadLine());
 
 Console.WriteLine(num1 + num2);
+
+// Add decimal values and whole numbers
+Console.Write("Enter a number: ");
+double decimal1 = double.Parse(Console.ReadLine());
+Console.Write("Enter another number: ");
+double decimal2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(decimal2 + decimal2);
 
 Console.ReadLine();
